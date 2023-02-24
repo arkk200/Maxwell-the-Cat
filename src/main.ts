c075ce9bf9b52c79cca2ab04509313116c81f077
leemyeongjae <leemyeongjae@leemyeongjaeui-MacBookPro.local>
2023-02-24 22:20:05 +0900

    소리도 입힘ㅁ

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import * as THREE from 'three';
 import { Color } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+
+alert('click to play sound');
 
 const 씬 = new THREE.Scene();
 씬.background = new Color(0xffffff);
@@ -21,8 +22,6 @@ var 렌더러 = new THREE.WebGLRenderer({ antialias: true });
 렌더러.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('app')?.appendChild(렌더러.domElement);
 
-new OrbitControls(카메라맨, 렌더러.domElement);
-
 // @ts-ignore
 var 고앵이멕스웨씨;
 
@@ -40,7 +39,6 @@ new GLTFLoader().load( '/dingus_the_cat_big.glb', function ( gltf ) {
   고앵이멕스웨씨?.rotateY(Math.PI / 2 - 92.95);
 	씬.add( 고앵이멕스웨씨! );
   로동하는_함수();
-  console.log(씬);
 
   function 로동하는_함수() {
     requestAnimationFrame(로동하는_함수);
@@ -49,6 +47,13 @@ new GLTFLoader().load( '/dingus_the_cat_big.glb', function ( gltf ) {
     고앵이멕스웨씨?.rotateY(-0.0187);
     렌더러.render(씬, 카메라맨);
   }
+
+  const 내5디5 = new Audio();
+  내5디5.muted = true;
+  내5디5.src = "/Maxwell The Cat cut6.mp3";
+  내5디5.loop = true;
+  window.addEventListener('click', () => 내5디5.play());
+  내5디5.muted = false;
 }, undefined, function ( 에러났니 ) {
 
 	console.error( 에러났니 );
